729c5fab08c57657f974708613ef0307a57fd9d0
Prem Je Kalister <151918796+premcai@users.noreply.github.com>
2025-04-04 13:49:38 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, jsonify
+from pymongo import MongoClient
+
+app = Flask(__name__)
+
+# MongoDB connection
+uri = "mongodb+srv://myapp_user:secureTest123@pii-dify.4inrrqu.mongodb.net/?retryWrites=true&w=majority"
+client = MongoClient(uri)
+db = client["dify_demo"]
+collection = db["users"]
+
+@app.route("/")
+def home():
+    return "✅ Flask API is running!"
+
+@app.route("/get-data", methods=["GET"])
+def get_data():
+    data = list(collection.find({}, {"_id": 0}))  # exclude _id for simplicity
+    return jsonify(data)
+
+if __name__ == "__main__":
+    app.run(debug=True)
