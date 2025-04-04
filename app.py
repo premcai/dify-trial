2d830a08177610f5b58663c384423723c77f3b23
Prem Je Kalister <151918796+premcai@users.noreply.github.com>
2025-04-04 14:01:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -19,4 +19,7 @@ def get_data():
     return jsonify(data)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
+
